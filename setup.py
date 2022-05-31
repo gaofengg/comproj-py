ad342cdc76c8069ee1bbd6890d75851e3a9e3e12
gaofengg <ymmtd@163.com>
2022-05-31 12:09:48 +0800

    change tag to 0.1.25

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="compyc",
-    version="0.1.24",
+    version="0.1.25",
     author="GaoFeng",
     author_email="ymmtd@163.com",
     long_description=long_description,
